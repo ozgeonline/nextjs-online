e5651f04df819858b6dc2d22f08204c6ad0a26bb
ozgeonline <nisroc.414@hotmail.com>
2024-12-09 14:04:59 +0300

    home page layout

--- app/home/layout.tsx
+++ app/home/layout.tsx
@@ -1,17 +1,17 @@
 import { getServerSession } from "next-auth"
+import { ReactNode } from "react"
 import { authOptions } from "../utils/auth"
 import { redirect } from "next/navigation"
 
-export default async function HomeLayout(){
+export default async function HomeLayout({children} : {children: ReactNode}){
   const session = await getServerSession(authOptions)
-
   if(!session) {
     redirect("/tr-en")
   }
 
   return (
     <div className="w-full">
-      Hello World!
+      {children}
     </div>
   )
 }
---
+++ app/home/page.tsx
@@ -0,0 +1,12 @@
+
+export default function HomePage() {
+  return (
+      <div className="overflow-hidden ">
+        Hello World
+      </div>
+  );
+}
+
+
+
+
